d4d1382f735a8a9c71781995336eeeb7eb21ae27
Rafael Sartori <faelsartori@gmail.com>
2011-10-24 10:35:17 -0200

    with dynamic alocation

--- shell.c
+++ shell.c
@@ -1,24 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int parseLine(char *v, char trix[40][40], int commandLine) {
+char *initializeVector() {
+	char *v;
+
+	v = (char *) malloc(sizeof(char)*41);
+	if(v == NULL){
+		printf("memory");
+	}
+
+	return v;
+}
+
+char **initializeMatrix(){
+	char **trix;
+	int i;
+	
+	trix = (char **) malloc (sizeof(char) * 41);
+	for(i=0; i < 41; i++){
+		trix[i] = (char *) malloc(sizeof(char) * 41);
+	}
+	return trix;
+}
+
+int parseLine(char *v, char **trix, int commandLine) {
 	int column;
 
 	for(column=0; v[column] != '\0'; column++){
 		trix[commandLine][column] = v[column];	
 	}
 		trix[commandLine][column] = '\0';
+
+	return 0;
 }
 
 int main(void) {
 	int i, j, loopProgram, commandLine;
-	char c, v[40];
-	char trix[40][40];
-
-	for(i=0; i < 40; i++){
-		for(j=0; j < 40; j++){
-			trix[i][j] = 'x';
-		} 
-	} 
+	char c, *v, **trix;
+	
+	v = initializeVector();
+	trix = initializeMatrix();
 
 	loopProgram = 1;
 	commandLine = 1;
