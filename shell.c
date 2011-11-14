13ebd6770f921882518f5409c717e6c400b7eb6a
Rafael Sartori <faelsartori@gmail.com>
2011-11-14 15:14:48 -0200

    With interpreter

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char *initializeVector() {
 	char *v;
@@ -35,17 +36,53 @@ int parseLine(char *v, char **trix, int commandLine) {
 	return 0;
 }
 
-/*
+int interpreter(char *v) {
+	int i, j, k;
+	char **aux, c, last, comm[20];
+	/* aux eh uma matrix auxiliar */
+	/* c guarda caracteres temporariamente */
+	/* last guarda a posicao do ultimo espaco */
 
-int interpreter(char **trix, int commandLine) {
-	int i;
-
-	for(i=0; i != '/0'; i++){
-		trix[i][commandLine]; // Nao terminado, parado na metade
+	aux = (char**) malloc (sizeof(char) * 41);
+	for(i=0; i < 41; i++) {
+		aux[i] = (char *) malloc(sizeof(char) * 41);
 	}
-}
 
-*/
+	last = 0;
+	k = 0;
+	
+	for(i=0; v[i] != '\0'; i++){
+		c = v[i];
+			
+		if(c == ' '){
+			for(j = last; j != i; j++) {
+				aux[k][j-last] = v[j]; 
+			}
+			aux[k][j] = '\0';
+			last = i+1;
+			k++;
+		}
+	}
+
+	for(i = last; v[i] != '\0'; i++) {
+		aux[k][i] = v[i];
+	}
+	aux[k][i] = '\0';
+	k++;
+
+	k--;
+	while(k >= 0){
+		for(j=0; aux[k][j] != '\0'; j++){
+			comm[j] = aux[k][j];
+		}
+		comm[j] = '\0';
+		k--;
+
+		printf("\n%s\n", comm);
+	}
+
+	return 0;
+}
 
 int main(void) {
 	int i, j, loopProgram, commandLine;
@@ -76,7 +113,7 @@ int main(void) {
 			printf("%c", trix[commandLine][i]);
 		}
 
-		// interpreter(trix, commandLine);
+		interpreter(v);
 				
 		commandLine++;			
 	}
