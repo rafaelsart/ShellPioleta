b4ad869c9993b485b5080fa21c9c0f4d1efe07d2
Fernando Maia <fsmaia@gmail.com>
2011-11-18 02:32:49 -0200

    Special Linked List Jobs - Fully Working

--- jobsListTest.c
+++ jobsListTest.c
@@ -6,6 +6,7 @@
 #define ERRO_SUCESSO 1
 #define ERRO_NAO_ENCONTRADO -1
 #define ERRO_NAO_ALOCADO -2
+#define ERRO_LISTA_VAZIA -3
 
 /* Status personalizados */
 #define BACKGROUND 10
@@ -77,25 +78,31 @@ int Jobs_removeJob(JobHeader *L, pid_t pid) {
 	if(jobInicio != NULL) {
 		//Remoção no início		
 		if(jobInicio->pid == pid) {
-			//Refaz a ligação do primeiro Job
+			//Salva o elemento a ser removido
+			jobAux = jobInicio;
+			//Atualiza a Lista
 			L->primeiroJob = jobInicio->prox;
 			//Lista de tamanho 1
 			if(L->numJobs == 1) L->ultimoJob = L->primeiroJob;
-			//Job não encontrado
-			return ERRO_NAO_ENCONTRADO;
+			//Libera a memória alocada
+			free(jobAux);
 		}
 		//Procura o ponto de remoção
 		else {
+			//Percorre o primeiro elemento da lista e salva o atual
+			jobAux = jobInicio;
+			jobInicio = jobInicio->prox;
 			while(jobInicio != NULL) {
-				if(jobInicio->prox->pid == pid) {
-					//Salva o elemento a ser removido
-					jobAux = jobInicio->prox;
+				if(jobInicio->pid == pid) {
+					//Último elemento da lista
+					if(jobInicio == L->ultimoJob) L->ultimoJob = jobAux;
 					//Atualiza a lista
-					jobInicio->prox = jobAux->prox;
+					jobAux->prox = jobInicio->prox;
 					//Libera a memória alocada
-					free(jobAux);
+					free(jobInicio);
 				}
 				//Percorre a lista
+				jobAux = jobInicio;
 				jobInicio = jobInicio->prox;
 			}
 			//Job não encontrado
@@ -103,12 +110,14 @@ int Jobs_removeJob(JobHeader *L, pid_t pid) {
 		}
 			
 	}
+	//Lista vazia
+	return ERRO_LISTA_VAZIA;
 }
 
 /* Coloca Job em Background */
 int Jobs_colocaJobEmBackground (JobHeader L, pid_t pid) {
 	//Armazena primeiro elemento da lista
-	Job *jobInicio = L.primeiroElemento;
+	Job *jobInicio = L.primeiroJob;
 	
 	while (jobInicio != NULL) {
 		//Procura Job desejado	       
@@ -125,10 +134,10 @@ int Jobs_colocaJobEmBackground (JobHeader L, pid_t pid) {
 	return ERRO_NAO_ENCONTRADO;
 }
 
-/* Coloca Job em Background */
-int Jobs_colocaJobEmBackground (JobHeader L, pid_t pid) {
+/* Coloca Job em Foreground */
+int Jobs_colocaJobEmForeground (JobHeader L, pid_t pid) {
 	//Armazena primeiro elemento da lista
-	Job *jobInicio = L.primeiroElemento;
+	Job *jobInicio = L.primeiroJob;
 	
 	while (jobInicio != NULL) {
 		//Procura Job desejado	       
@@ -148,7 +157,7 @@ int Jobs_colocaJobEmBackground (JobHeader L, pid_t pid) {
 /* Retorna PID do Job em Foreground */
 pid_t Jobs_retornaJobEmForeground(JobHeader L) {
 	//Armazena primeiro elemento da lista
-	Job *jobInicio = L.primeiroElemento;
+	Job *jobInicio = L.primeiroJob;
 	while (jobInicio != NULL) {
 		//Procura Job desejado
 		if(jobInicio->status == FOREGROUND) return jobInicio->pid;
@@ -162,13 +171,14 @@ pid_t Jobs_retornaJobEmForeground(JobHeader L) {
 /* Imprime Lista de Jobs na Tela */
 void Jobs_imprimeJobs(JobHeader L) {
 	//Variáveis
-	int iContador;
+	int iContador = 1;
 	//Armazena primeiro elemento da lista
-	Job *jobInicio = L.primeiroElemento;
+	Job *jobInicio = L.primeiroJob;
 	//Lista vazia
 	if(jobInicio == NULL) printf("\nLista vazia!\n");
 	else {	
 		while (jobInicio != NULL) {
+			//Imprime lista formatada
 			printf("\n");
 			//Título
 			printf("Job #%d\n", iContador);
@@ -179,7 +189,10 @@ void Jobs_imprimeJobs(JobHeader L) {
 			if(jobInicio->status==BACKGROUND) printf("Background\n");
 			else if(jobInicio->status==FOREGROUND) printf("Foreground\n");
 			else printf("Desconhecido\n");
+			//Percorre a lista
 			jobInicio = jobInicio->prox;
+			//Incrementa o contador
+			iContador++;
 		}
 	}
 }
@@ -188,19 +201,68 @@ int main(void) {
 	//Variáveis
 	JobHeader L;
 	Job *J;
+	
 	//Atributos de L
-	L->numJobs = 0;
-	L->primeiroJob = NULL;
-	L->ultimoJob = NULL;
-	//Comandos
-	J = L.primeiroElemento;
+	L.numJobs = 0;
+	L.primeiroJob = NULL;
+	L.ultimoJob = NULL;
+	
+	//Stress test
+	J = L.primeiroJob;
+	
+	//Remove Job Inexistente
+	Jobs_removeJob(&L,12);
+
+	//Cria Lista/Adiciona Job
 	Jobs_adicionaJob(&L,12,FOREGROUND);
+	
+	//Adiciona Jobs
 	Jobs_adicionaJob(&L,14,BACKGROUND);
-	Jobs_adicionaJob(&L,18,BACKGROUND);
+	Jobs_adicionaJob(&L,18,FOREGROUND);
 	Jobs_adicionaJob(&L,11,FOREGROUND);
+	Jobs_adicionaJob(&L,15,BACKGROUND);
 	Jobs_adicionaJob(&L,9,BACKGROUND);
 	Jobs_adicionaJob(&L,7,FOREGROUND);
-	Jobs_imprimeJobs(L);
+	
+	
+	//Remove Job do início da Lista
+	Jobs_removeJob(&L,12);
+
+	
+	//Remove Job do final da Lista
+	Jobs_removeJob(&L,7);
+	//Remove Job do meio da Lista
+	Jobs_removeJob(&L,9);
+	//Remove Job inexistente
+	Jobs_removeJob(&L,410);
+	
+	//Coloca Job em Foreground (cujo status é Foreground)
+	Jobs_colocaJobEmForeground(L,18);
+	//Coloca Job em Foreground (cujo status é Background)
+	Jobs_colocaJobEmForeground(L,14);
+	//Coloca Jobs Já Removidas em Foreground
+	Jobs_colocaJobEmForeground(L,7);
+	Jobs_colocaJobEmForeground(L,9);
+	Jobs_colocaJobEmForeground(L,12);
+	
+
+	//Coloca Job em Background (cujo status é Background)
+	Jobs_colocaJobEmBackground(L,15);
+	//Coloca Job em Background (cujo status é Foreground)
+	Jobs_colocaJobEmBackground(L,18);
+	Jobs_colocaJobEmBackground(L,14);
+	//Coloca Jobs Já Removidas em Background
+	Jobs_colocaJobEmBackground(L,7);
+	Jobs_colocaJobEmBackground(L,9);
+	Jobs_colocaJobEmBackground(L,12);
+	
+	//Retorna PID da Job em Foreground
+	printf("\n\nJob em Foreground: %d\n\n",Jobs_retornaJobEmForeground(L)); /* 14 */
+
+
+	//Imprime Jobs
+	Jobs_imprimeJobs(	L);
+	//Retorno
 	return 1;
 }
 	
